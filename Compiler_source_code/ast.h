e4250ebab370246b7c58c69c6256a4e3363703c0
Dogwalker-kryt <215691531+Dogwalker-kryt@users.noreply.github.com>
2025-07-07 17:19:47 +0200

    Update ast.h

--- Compiler_source_code/ast.h
+++ Compiler_source_code/ast.h
@@ -1,37 +1,39 @@
+#ifndef AST_H
+#define AST_H
+
 typedef enum {
-    AST_FUNCTION_DEF,
     AST_RETURN_STMT,
+    AST_FUNCTION_DEF,
     AST_IDENTIFIER,
-    AST_NUMBER,
     AST_BINARY_OP
 } ASTNodeType;
 
-typedef struct ASTNode {
+typedef struct ASTNode ASTNode;
+
+struct ASTNode {
     ASTNodeType type;
     union {
         struct {
-            char name[64];
-            char arg1[64];
-            char arg2[64];
-            struct ASTNode* body;
-        } function_def;
-
-        struct {
-            struct ASTNode* expr;
-        } return_stmt;
-
-        struct {
-            struct ASTNode* left;
-            char op[8];
-            struct ASTNode* right;
-        } binary_op;
-
-        struct {
-            char name[64];
+            char name[256];
         } identifier;
 
         struct {
-            int value;
-        } number;
+            ASTNode* left;
+            char op[8];
+            ASTNode* right;
+        } binary_op;
+
+        struct {
+            ASTNode* expr;
+        } return_stmt;
+
+        struct {
+            char name[256];
+            char arg1[256];
+            char arg2[256];
+            ASTNode* body;
+        } function_def;
     };
-} ASTNode;
+};
+
+#endif
